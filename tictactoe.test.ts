861e95440ff388a7a10627f94c670af195248906
Gunnar Huscroft <gunnarhuscroft@gmail.com>
2021-10-08 10:20:26 -0600

    done

--- tictactoe.test.ts
+++ tictactoe.test.ts
@@ -12,9 +12,51 @@ afterAll(async () => {
     await driver.quit()
 })
 
+function delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+}
+
 test('I can start a game', async () => {
 
     let button = await (await driver).findElement(By.id('start-game'));
     await button.click();
+    
+});
+
+test('I can add an X to the top row', async () => {
+
+    let button = await (await driver).findElement(By.id('cell-0'));
+    await button.click();
+    await delay(1000);
+    
+});
+
+test('I can add a second X to the top row', async () => {
+
+    let button = await (await driver).findElement(By.id('cell-1'));
+    let button1 = await (await driver).findElement(By.id('cell-2'));
+
+    if(button.getText.toString() == 'O') {
+        await button.click();
+    } else {
+        button1.click();
+    }
+
+    await delay(1000);
+    
+});
+
+test('I can add an X to the middle Row', async () => {
+
+    let button = await (await driver).findElement(By.id('cell-4'));
+    let button1 = await (await driver).findElement(By.id('cell-5'));
+
+    if(button.getText.toString() == 'O') {
+        await button.click();
+    } else {
+        button1.click();
+    }
+
+    await delay(1000);
     
 });
